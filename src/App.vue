5f836e865b82609b0abaacd0fd059e2c91cab404
Laura Golec <golecl@tcd.ie>
2023-10-07 17:13:08 +0100

    added first content
    added a hello world message and a global scss style along with setting up the webpack

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div>
+        <h1>Hello World!</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello from Laura',
+        };
+    },
+};
+</script>
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import * as Vue from "vue";
+import * as VueRouter from "vue-router";
+import './styles/app.scss'; 
+import App from "./App.vue";
+
+const routes = [{ path: "/", component: App }];
+const router = VueRouter.createRouter({
+  routes,
+  history: VueRouter.createWebHistory(),
+});
+
+const app = Vue.createApp(App);
+app.use(router);
+app.mount("#root");
